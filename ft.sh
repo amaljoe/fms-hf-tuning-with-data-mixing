f6f554995e9f03e01fc3878047c70b889827249a
amaljoe <amaljoe88@gmail.com>
2025-03-04 23:37:07 +0530

    custom data formatter

--- ft.sh
+++ ft.sh
@@ -1,9 +1,7 @@
 # if you want to use one GPU on multi-gpu machine
 export CUDA_VISIBLE_DEVICES=0
 
- MODEL_PATH=base_models/granite-3.2-2b-instruct
- TRAIN_DATA_PATH=data/trans.jsonl
- OUTPUT_PATH=out
+ # k_proj v_proj o_proj gate_proj up_proj down_proj
 
 python tuning/sft_trainer.py  \
 --model_name_or_path base_models/granite-3.2-2b-instruct \
@@ -14,12 +12,12 @@ python tuning/sft_trainer.py  \
 --gradient_accumulation_steps 4 \
 --learning_rate 1e-4 \
 --torch_dtype bfloat16 \
---response_template "<|assistant|>" \
---instruction_template "<|user|>" \
+--response_template "<|start_of_role|>assistant<|end_of_role|>" \
+--data_formatter_template "<|start_of_role|>system<|end_of_role|>Translate the Malayalam text to English.<|end_of_text|>\n<|start_of_role|>user<|end_of_role|>{{input}}<|end_of_text|>\n<|start_of_role|>assistant<|end_of_role|>{{output}}<|end_of_text|>\n" \
 --peft_method lora \
 --lora_r 8 \
 --lora_alpha 16 \
 --lora_dropout 0.05 \
---target_modules "all-linear" \
+--target_modules q_proj k_proj v_proj o_proj gate_proj up_proj down_proj \
 --save_strategy epoch \
 --save_total_limit 1
